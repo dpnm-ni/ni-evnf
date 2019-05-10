bd3bed710ec4e9a32ef4921fdad931251ed8034e
Ubuntu <ubuntu@efw.novalocal>
2019-05-10 02:28:38 +0000

    basic code for forwarding

---
+++ efw/efw.c
@@ -0,0 +1,83 @@
+#define KBUILD_MODNAME "efw"
+#include <linux/in.h>
+#include <linux/if_ether.h>
+#include <linux/if_packet.h>
+#include <linux/if_vlan.h>
+#include <linux/ip.h>
+#include <linux/ipv6.h>
+
+#define ETHTYPE_IP 0x0800
+
+// get from cflags
+// detail: https://stackoverflow.com/questions/25254043/is-it-
+// possible-to-compare-ifdef-values-for-conditional-use
+#define LOCAL_MAC _LOCAL_MAC
+
+#define htonll(_num) (__builtin_bswap64(_num) >> 16)
+
+#define CURSOR_ADVANCE(_target, _cursor, _len,_data_end) \
+    ({  _target = _cursor; _cursor += _len; \
+        if(_cursor > _data_end) return XDP_DROP; })
+
+struct eth_tp {
+    u64 dst:48;
+    u64 src:48;
+    u16 type;
+} __attribute__((packed));
+
+static inline u16 checksum(u16 *buf, int bufsz) {
+    u32 sum = 0;
+
+    while (bufsz > 1) {
+        sum += *buf;
+        buf++;
+        bufsz -= 2;
+    }
+
+    if (bufsz == 1) {
+        sum += *(u8 *)buf;
+    }
+
+    sum = (sum & 0xffff) + (sum >> 16);
+    sum = (sum & 0xffff) + (sum >> 16);
+
+    return ~sum;
+}
+
+BPF_TABLE("hash",u32, u64, tb_ip_mac, 1024);
+BPF_PERF_OUTPUT(events);
+
+int fw(struct xdp_md *ctx) {
+    void* data_end = (void*)(long)ctx->data_end;
+    void* cursor = (void*)(long)ctx->data;
+
+     struct eth_tp *eth;
+    CURSOR_ADVANCE(eth, cursor, sizeof(*eth), data_end);
+    
+    if (ntohs(eth->type) != ETHTYPE_IP)
+        return XDP_PASS;
+    
+    struct iphdr *ip;
+    CURSOR_ADVANCE(ip, cursor, sizeof(*ip), data_end);
+
+    if (ip->protocol != IPPROTO_TCP)
+        return XDP_PASS;
+
+    u32 dst_ip = ntohl(ip->daddr);
+    u64 dst_mac = 0;
+    u64 *dst_mac_p = tb_ip_mac.lookup(&dst_ip);
+    if (!dst_mac_p) {
+        events.perf_submit(ctx, &dst_ip, sizeof(dst_ip));
+        return XDP_PASS;
+    }
+
+    eth->src = htonll(LOCAL_MAC);
+    eth->dst = *dst_mac_p;
+
+    // reduce ttl and compute checksum
+    ip->ttl = ip->ttl - 1;
+    ip->check = 0;
+    ip->check = checksum((u16 *)ip, sizeof(struct iphdr));
+
+    return XDP_TX;
+}
---
+++ efw/efw.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+
+import time
+import sys
+from bcc import BPF
+from ipaddress import IPv4Address
+from getmac import get_mac_address
+
+
+class EFw(object):
+    """docstring for EFw"""
+    def __init__(self, iface):
+        super(EFw, self).__init__()
+        self.iface = iface
+        self.LOCAL_MAC = self.mac_str_to_int(get_mac_address(interface=iface))
+        
+        self.bpf_fw = BPF(src_file="efw.c", debug=0,
+            cflags=["-w", "-D_LOCAL_MAC=%s" % self.LOCAL_MAC])
+
+        self.fn_fw = self.bpf_fw.load_func("fw", BPF.XDP)
+
+        self.tb_ip_mac = self.bpf_fw.get_table("tb_ip_mac")
+
+    def mac_str_to_int(self, mac_str):
+        mac_arr = mac_str.split(':')
+        tmp =""
+        for i in range(0, 6):
+            tmp += mac_arr[i]
+        return int(tmp, 16)
+
+    def set_tb_ip_mac(self, ip, mac):
+        k = self.tb_ip_mac.Key(ip)
+        leaf = self.tb_ip_mac.Leaf(mac)
+        self.tb_ip_mac[k] = leaf
+
+    def attach_iface(self):
+        self.bpf_fw.attach_xdp(self.iface, self.fn_fw, 0)
+
+    def detach_iface(self):
+        self.bpf_fw.remove_xdp(self.iface, 0)
+
+    def open_events(self):
+        def _process_event(ctx, data, size):
+            class Event(ct.Structure):
+                _fields_ =  [("dst_ip", ct.c_uint32)]
+
+            event = ct.cast(data, ct.POINTER(Event)).contents
+            dst_ip_str = str(IPv4Address(event.dst_ip)) 
+            dst_mac_str = get_mac_address(dst_ip_str)
+            self.set_tb_ip_mac(int(event.dst_ip), mac_str_to_int(dst_mac_str))
+            print "IP to MAC: ", event.dst_ip, " - ", dst_mac_str
+
+        self.bpf_fw["events"].open_perf_buffer(_process_event, page_cnt=512)
+
+    def poll_events(self):
+        self.bpf_fw.kprobe_poll()
+
+
+if __name__ == "__main__":
+    iface = "ens4"
+
+    efw = EFw(iface)
+    efw.attach_iface()
+    efw.open_events()
+
+    print "eBPF prog Loaded"
+    sys.stdout.flush()
+
+    try:
+        while 1:
+            efw.poll_events()
+
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        efw.detach_iface()
+        print "Done"
