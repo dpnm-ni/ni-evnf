9b544fbca908c2b3d26e4d672a17e85474861a41
tu-nv <vantu.bkhn@gmail.com>
2019-05-17 07:15:50 +0000

    elb (WIP)

--- elb/elb.c
+++ elb/elb.c
@@ -7,6 +7,7 @@
 #include <linux/ipv6.h>
 
 #define ETHTYPE_IP 0x0800
+#define HTTPTYPE_TCP 80
 
 // get from cflags
 // detail: https://stackoverflow.com/questions/25254043/is-it-
@@ -25,6 +26,13 @@ struct eth_tp {
     u64 src:48;
     u16 type;
 } __attribute__((packed));
+
+struct flow_id_t {
+    u32 src_ip;
+    u32 dst_ip;
+    u16 src_port;
+    u16 dst_port;
+}; __attribute__((packed));
 
 // static inline u16 checksum(u16 *buf, int bufsz) {
 //     u32 sum = 0;
@@ -48,11 +56,15 @@ struct eth_tp {
 BPF_TABLE("hash",u32, u64, tb_ip_mac, 1024);
 BPF_PERF_OUTPUT(events);
 
-int fw(struct xdp_md *ctx) {
+int lb(struct xdp_md *ctx) {
     void* data_end = (void*)(long)ctx->data_end;
     void* cursor = (void*)(long)ctx->data;
 
-     struct eth_tp *eth;
+    u32 sum;
+    u16 ip_sub_old;
+    u16 ip_sub_new;
+
+    struct eth_tp *eth;
     CURSOR_ADVANCE(eth, cursor, sizeof(*eth), data_end);
     
     if (ntohs(eth->type) != ETHTYPE_IP)
@@ -61,11 +73,60 @@ int fw(struct xdp_md *ctx) {
     struct iphdr *ip;
     CURSOR_ADVANCE(ip, cursor, sizeof(*ip), data_end);
 
-    u32 dst_ip = ntohl(ip->daddr);
-    if (dst_ip == LOCAL_IP)
+
+    if (ip->protocol != IPPROTO_TCP)
         return XDP_PASS;
 
+    struct tcphdr *tcp;
+    CURSOR_ADVANCE(tcp, cursor, sizeof(*tcp), data_end);
+    if(ntohs(tcp->dest) != HTTPTYPE_TCP && ntohs(tcp->source) != HTTPTYPE_TCP)
+        return XDP_PASS;
+
+    struct flow_id_t flow_id = {};
+    flow_id.src_ip = ntohl(ip->saddr);
+    flow_id.dst_ip = ntohl(ip->daddr);
+    flow_id.src_port = ntohs(tcp->source);
+    flow_id.dst_port = ntohs(tcp->dest);
+
+
+    /* new ip */
+
+    if (ntohl(ip->daddr) == LOCAL_IP) {
+        // to server
+        ip_sub_old = ntohs(ip->daddr >> 16);
+        ip->daddr = htonl(3232236552); // 192.168.4.8
+        ip_sub_new = ntohs(ip->daddr >> 16);
+    } else {
+        // to client
+        ip_sub_old = ntohs(ip->saddr >> 16);
+        ip->saddr = htonl(LOCAL_IP);
+        ip_sub_new = ntohs(ip->saddr >> 16);
+    }
+
+
+
+    /* recompute checksum */
+
+    // ip checksum
+    u32 sum_new = ntohs(ip->check) + ip_sub_old + ~ip_sub_new;
+    sum_new = (sum_new & 0xffff) + (sum_new >> 16);
+    ip->check = htons((sum_new & 0xffff) + (sum_new >> 16) + 1);
+
+    // ip->check = 0;
+    // u16 sum_correct = checksum((u16 *)ip, sizeof(struct iphdr));
+    // if (sum_new - sum_correct != 0)
+    //     bpf_trace_printk("sum new: %u, correct: %u, proto %d\n",
+    //         sum_new, sum_correct, ip->protocol);
+
+    // tcpchecksum
+    sum_new = ntohs(tcp->check) + ip_sub_old + ~ip_sub_new;
+    sum_new = (sum_new & 0xffff) + (sum_new >> 16);
+    tcp->check = htons((sum_new & 0xffff) + (sum_new >> 16) + 1);
+
+    /* Forwarding */
+
     u64 dst_mac = 0;
+    u32 dst_ip = ntohl(ip->daddr);
     u64 *dst_mac_p = tb_ip_mac.lookup(&dst_ip);
     if (!dst_mac_p) {
         events.perf_submit(ctx, &dst_ip, sizeof(dst_ip));
@@ -74,11 +135,6 @@ int fw(struct xdp_md *ctx) {
 
     eth->src = htonll(LOCAL_MAC);
     eth->dst = htonll(*dst_mac_p);
-
-    // reduce ttl and compute checksum
-    // ip->ttl = ip->ttl - 1;
-    // ip->check = 0;
-    // ip->check = checksum((u16 *)ip, sizeof(struct iphdr));
 
     return XDP_TX;
 }
--- elb/elb.py
+++ elb/elb.py
@@ -9,10 +9,10 @@ from ipaddress import IPv4Address
 from getmac import get_mac_address
 
 
-class EFw(object):
-    """docstring for EFw"""
+class ELB(object):
+    """docstring for ELB"""
     def __init__(self, iface):
-        super(EFw, self).__init__()
+        super(ELB, self).__init__()
         self.iface = iface
 
         _local_ip_str = ni.ifaddresses(iface)[ni.AF_INET][0]['addr']
@@ -21,14 +21,14 @@ class EFw(object):
         _local_mac_str = get_mac_address(interface=iface)
         self.LOCAL_MAC = self.mac_str_to_int(_local_mac_str)
         
-        self.bpf_fw = BPF(src_file="efw.c", debug=0,
+        self.bpf_lb = BPF(src_file="elb.c", debug=0,
             cflags=["-w",
                     "-D_LOCAL_IP=%s" % self.LOCAL_IP,
                     "-D_LOCAL_MAC=%s" % self.LOCAL_MAC])
 
-        self.fn_fw = self.bpf_fw.load_func("fw", BPF.XDP)
+        self.fn_lb = self.bpf_lb.load_func("lb", BPF.XDP)
 
-        self.tb_ip_mac = self.bpf_fw.get_table("tb_ip_mac")
+        self.tb_ip_mac = self.bpf_lb.get_table("tb_ip_mac")
 
     def mac_str_to_int(self, mac_str):
         mac_arr = mac_str.split(':')
@@ -43,10 +43,10 @@ class EFw(object):
         self.tb_ip_mac[k] = leaf
 
     def attach_iface(self):
-        self.bpf_fw.attach_xdp(self.iface, self.fn_fw, 0)
+        self.bpf_lb.attach_xdp(self.iface, self.fn_lb, 0)
 
     def detach_iface(self):
-        self.bpf_fw.remove_xdp(self.iface, 0)
+        self.bpf_lb.remove_xdp(self.iface, 0)
 
     def open_events(self):
         def _process_event(ctx, data, size):
@@ -56,32 +56,32 @@ class EFw(object):
             event = ct.cast(data, ct.POINTER(Event)).contents
             dst_ip_str = str(IPv4Address(event.dst_ip)) 
             dst_mac_str = get_mac_address(ip=dst_ip_str)
-            self.set_tb_ip_mac(int(event.dst_ip), self.mac_str_to_int(dst_mac_str))
             print "IP to MAC: ", event.dst_ip, " - ", dst_mac_str
+            self.set_tb_ip_mac(int(event.dst_ip), self.mac_str_to_int(dst_mac_str))
 
-        self.bpf_fw["events"].open_perf_buffer(_process_event, page_cnt=512)
+        self.bpf_lb["events"].open_perf_buffer(_process_event, page_cnt=512)
 
     def poll_events(self):
-        self.bpf_fw.kprobe_poll()
+        self.bpf_lb.kprobe_poll()
 
 
 if __name__ == "__main__":
     iface = "ens4"
 
-    efw = EFw(iface)
-    efw.attach_iface()
-    efw.open_events()
+    elb = ELB(iface)
+    elb.attach_iface()
+    elb.open_events()
 
     print "eBPF prog Loaded"
     sys.stdout.flush()
 
     try:
         while 1:
-            efw.poll_events()
+            elb.poll_events()
 
     except KeyboardInterrupt:
         pass
 
     finally:
-        efw.detach_iface()
+        elb.detach_iface()
         print "Done"
