b638afdba016365ba89e3a51cc3b62c368cf2464
tu-nv <vantu.bkhn@gmail.com>
2019-06-25 05:43:26 +0000

    update lb periodically update server load refactor code

--- efwlb/efwlb.py
+++ efwlb/efwlb.py
@@ -1,47 +1,68 @@
+import argparse
+import threading
+import time
+
 import sys
 sys.path.append("../")
-import argparse
-
 from efw.efw import EFW
 from elb.elb import ELB
 
-parser = argparse.ArgumentParser()
-parser.add_argument("iface", help="iface to listen")
-parser.add_argument("servers", nargs='+', type=unicode, help="servers to load balance")
-args = parser.parse_args()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--period", default=60, type=int, help="period to update server load in seconds")
+    parser.add_argument("iface", help="iface to listen")
+    parser.add_argument("servers", nargs='+', type=unicode, help="servers to load balance")
+    args = parser.parse_args()
 
-efw = EFW(args.iface, "../efw/efw.c")
-elb = ELB(args.iface, args.servers, "../elb/elb.c")
-
-s_frees = elb.get_servers_load()
-print "s_frees: ", s_frees
-s_weights_w_load = elb.cal_s_weights_w_load(s_frees)
-print "s_weights_w_load: ", s_weights_w_load
-new_s_hashmap = elb.cal_s_hashmap(s_weights_w_load)
-print "new_s_hashmap: ", new_s_hashmap
-elb.update_s_hashmap(new_s_hashmap)
-
-efw.attach_iface()
-efw.set_next_vnf(elb.fn_lb.fd)
+    efw = EFW(args.iface, "../efw/efw.c")
+    elb = ELB(args.iface, args.servers, "../elb/elb.c")
 
 
-efw.add_port(80, "allow")
-efw.add_allow_subnet((24, (192, 168, 4, 0)), 1)
-# k = efw.tb_subnet_allow.Key(24, (192, 168, 4, 4))
-# print "val: ", efw.tb_subnet_allow[k]
+    efw.attach_iface()
+    efw.set_next_vnf(elb.fn_lb.fd)
 
-elb.open_events()
 
-print "eBPF prog Loaded"
-sys.stdout.flush()
+    efw.add_port(80, "allow")
+    efw.add_allow_subnet((24, (192, 168, 4, 0)), 1)
+    # k = efw.tb_subnet_allow.Key(24, (192, 168, 4, 4))
+    # print "val: ", efw.tb_subnet_allow[k]
 
-try:
-    while 1:
-        elb.poll_events()
+    elb.open_events()
 
-except KeyboardInterrupt:
-    pass
+    print "eBPF prog Loaded"
+    sys.stdout.flush()
 
-finally:
-    efw.detach_iface()
-    print "Done"
+     # a separated thread to recaculate server distribution based on new server weights
+    to_stop = threading.Event()
+
+    def sleep_with_stopcheck(seconds):
+        remain = seconds
+        while not to_stop.is_set():
+            time.sleep(1)
+            remain = remain - 1
+            if remain == 0:
+                return
+
+    def update_server_map():
+        try:
+            while not to_stop.is_set():
+                elb.update_server_map()
+                sleep_with_stopcheck(args.period)
+        except Exception as e:
+            print(e)
+
+    update_server_map_thread = threading.Thread(target=update_server_map)
+    update_server_map_thread.start()
+
+    try:
+        while True:
+            elb.poll_events()
+
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        efw.detach_iface()
+        to_stop.set()
+        update_server_map_thread.join()
+        print "Done"
--- elb/elb.py
+++ elb/elb.py
@@ -15,20 +15,19 @@ from pysnmp.hlapi import *
 
 class ELB(object):
     """docstring for ELB"""
-    def __init__(self, iface, s_ips_str, bpf_src="elb.c"):
+    def __init__(self, iface, servers, bpf_src="elb.c"):
         super(ELB, self).__init__()
         self.iface = iface
-        self.s_ips_str = s_ips_str
-        self.s_ips = [int(IPv4Address(s_ip_str)) for s_ip_str in s_ips_str]
+        self.servers = [IPv4Address(server) for server in servers]
         self.HASHMAP_SIZE = 256
-        self.s_weights = [self.HASHMAP_SIZE/len(s_ips_str) for _ in range(0, len(s_ips_str))]
-        self.s_frees = [0 for _ in range(0, self.HASHMAP_SIZE)]
+        self.server_weights = [self.HASHMAP_SIZE/len(self.servers) for _ in range(0, len(self.servers))]
+        self.server_frees = [0 for _ in range(0, self.HASHMAP_SIZE)]
 
         _local_ip_str = ni.ifaddresses(iface)[ni.AF_INET][0]['addr']
         self.LOCAL_IP = int(IPv4Address(_local_ip_str))
 
         _local_mac_str = get_mac_address(interface=iface)
-        self.LOCAL_MAC = self.mac_str_to_int(_local_mac_str)
+        self.LOCAL_MAC = self._mac_str_to_int(_local_mac_str)
         
         self.bpf_lb = BPF(src_file=bpf_src, debug=0,
             cflags=["-w",
@@ -41,7 +40,7 @@ class ELB(object):
         self.tb_ip_mac = self.bpf_lb.get_table("tb_ip_mac")
         self.tb_server_ips = self.bpf_lb.get_table("tb_server_ips")
 
-    def mac_str_to_int(self, mac_str):
+    def _mac_str_to_int(self, mac_str):
         mac_arr = mac_str.split(':')
         tmp =""
         for i in range(0, 6):
@@ -68,7 +67,7 @@ class ELB(object):
             dst_ip_str = str(IPv4Address(event.dst_ip)) 
             dst_mac_str = get_mac_address(ip=dst_ip_str)
             if dst_mac_str is not None:
-                self.set_tb_ip_mac(int(event.dst_ip), self.mac_str_to_int(dst_mac_str))
+                self.set_tb_ip_mac(int(event.dst_ip), self._mac_str_to_int(dst_mac_str))
                 print "IP to MAC: ", event.dst_ip, " - ", dst_mac_str
             else:
                 print "warning: fail to get mac of: ", dst_ip_str
@@ -78,14 +77,14 @@ class ELB(object):
     def poll_events(self):
         self.bpf_lb.kprobe_poll()
 
-    def get_servers_load(self):
-        numServers = len(self.s_ips_str)
-        s_frees = [0 for i in range(0, numServers)]
+    def _get_servers_load(self):
+        numServers = len(self.servers)
+        server_frees = [0 for i in range(0, numServers)]
         for i in range(0, numServers):
             errorIndication, errorStatus, errorIndex, varBinds = next(
                 getCmd(SnmpEngine(),
                     CommunityData('public', mpModel=0),
-                    UdpTransportTarget((self.s_ips_str[i], 161)),
+                    UdpTransportTarget((str(self.servers[i]), 161)),
                     ContextData(),
                     ObjectType(ObjectIdentity('.1.3.6.1.4.1.2021.10.1.3.1')) ) )
 
@@ -96,54 +95,52 @@ class ELB(object):
                     errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
             else:
                 oid, val = varBinds[0]
-                s_frees[i] = 10 - min(10, int(round(10*float(val))))
-        return s_frees
+                server_frees[i] = 10 - min(10, int(round(10*float(val))))
+        return server_frees
 
-    def cal_s_weights_w_load(self, s_frees):
-        s_weights_w_load = [0 for _ in range(0, len(self.s_weights))]
-        for i in range(0, len(self.s_weights)):
-            s_weights_w_load[i] = self.s_weights[i] * s_frees[i]
-        return s_weights_w_load
+    def _cal_server_weights(self, server_frees):
+        server_weights = [0 for _ in range(0, len(self.server_weights))]
+        for i in range(0, len(self.server_weights)):
+            server_weights[i] = self.server_weights[i] * server_frees[i]
+        return server_weights
 
-    def cal_s_hashmap(self, s_weights_w_load):
-        new_s_hashmap = []
-        sum_weight = sum(s_weights_w_load)
-        for i in range(0, len(self.s_ips_str)):
-            num_of_sched = max(1, int(s_weights_w_load[i] * self.HASHMAP_SIZE / sum_weight))
-            new_s_hashmap.extend([self.s_ips[i] for _ in range(0, num_of_sched)])
+    def _cal_server_map(self, server_weights):
+        new_server_map = []
+        sum_weight = sum(server_weights)
+        for i in range(0, len(self.servers)):
+            num_of_sched = max(1, int(server_weights[i] * self.HASHMAP_SIZE / sum_weight))
+            new_server_map.extend([int(self.servers[i]) for _ in range(0, num_of_sched)])
 
-        if len(new_s_hashmap) < self.HASHMAP_SIZE:
-            new_s_hashmap.extend([self.s_ips[-1] for _ in range(len(new_s_hashmap), self.HASHMAP_SIZE)])
+        if len(new_server_map) < self.HASHMAP_SIZE:
+            new_server_map.extend([self.server_ips[-1] for _ in range(len(new_server_map), self.HASHMAP_SIZE)])
 
-        random.shuffle(new_s_hashmap)
-        return new_s_hashmap
-        # return random.shuffle(new_s_hashmap)
+        random.shuffle(new_server_map)
+        return new_server_map
+        # return random.shuffle(new_server_map)
 
-    def update_s_hashmap(self, new_s_hashmap):
-        for i in range(0, len(new_s_hashmap)):
+    def update_server_map(self):
+        server_frees = self._get_servers_load()
+        server_weights = self._cal_server_weights(server_frees)
+        new_server_map = self._cal_server_map(server_weights)
+
+        for i in range(0, len(new_server_map)):
             k = self.tb_server_ips.Key(i)
-            leaf = self.tb_server_ips.Leaf(new_s_hashmap[i])
-            # print "update mapping: ", i, ": ", new_s_hashmap[i]
+            leaf = self.tb_server_ips.Leaf(new_server_map[i])
+            # print "update mapping: ", i, ": ", new_server_map[i]
             self.tb_server_ips[k] = leaf
-        self.new_s_hashmap = new_s_hashmap
+        self.new_server_map = new_server_map
+
+        print "server_weights: ", server_weights
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--period", default=60, type=int, help="period to update server load in seconds")
     parser.add_argument("iface", help="iface to listen")
     parser.add_argument("servers", nargs='+', type=unicode, help="servers to load balance")
     args = parser.parse_args()
 
     elb = ELB(args.iface, args.servers)
-    s_frees = elb.get_servers_load()
-    print "s_frees: ", s_frees
-    s_weights_w_load = elb.cal_s_weights_w_load(s_frees)
-    print "s_weights_w_load: ", s_weights_w_load
-    new_s_hashmap = elb.cal_s_hashmap(s_weights_w_load)
-    print "new_s_hashmap: ", new_s_hashmap
-    elb.update_s_hashmap(new_s_hashmap)
-
-    # exit()
 
     elb.attach_iface()
     elb.open_events()
@@ -151,17 +148,27 @@ if __name__ == "__main__":
     print "eBPF prog Loaded"
     sys.stdout.flush()
 
-    # a separated thread to recaculate server distribution
-    # based on new server weights
-    # def _event_poll():
-    #     try:
-    #         while True:
-    #             edpi.poll_events()
-    #     except:
-    #         pass
-    # event_poll = threading.Thread(target=_event_poll)
-    # event_poll.daemon = True
-    # event_poll.start()
+    # a separated thread to recaculate server distribution based on new server weights
+    to_stop = threading.Event()
+
+    def sleep_with_stopcheck(seconds):
+        remain = seconds
+        while not to_stop.is_set():
+            time.sleep(1)
+            remain = remain - 1
+            if remain == 0:
+                return
+
+    def update_server_map():
+        try:
+            while not to_stop.is_set():
+                elb.update_server_map()
+                sleep_with_stopcheck(args.period)
+        except Exception as e:
+            print(e)
+
+    update_server_map_thread = threading.Thread(target=update_server_map)
+    update_server_map_thread.start()
 
     try:
         while True:
@@ -172,4 +179,6 @@ if __name__ == "__main__":
 
     finally:
         elb.detach_iface()
+        to_stop.set()
+        update_server_map_thread.join()
         print "Done"
