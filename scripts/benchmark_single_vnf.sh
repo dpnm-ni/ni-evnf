94fd9dd2e8a87a5f1f63d4316d7390c67805cabd
tu-nv <vantu.bkhn@gmail.com>
2020-02-10 02:59:43 +0000

    use clone skb to increase pktgen perf

--- scripts/benchmark_single_vnf.sh
+++ scripts/benchmark_single_vnf.sh
@@ -7,37 +7,41 @@
 set -e
 set -x
 
-VNF_SSH_ADDR="ubuntu@10.10.10.2"
-VNF_MAC="62:85:56:1b:e2:13"
-OUT_NIC="enp5s0f0"
-NUM_THREADS=8
-PKT_SIZE=64
-MEASUREMENT_TIME=120
-REST_TIME=8
+VNF_SSH_ADDR="ubuntu@10.10.10.10"
+VNF_MAC="fa:16:3e:52:46:1d"
+OUT_NIC="ens4"
+NUM_THREADS=1
+PKT_SIZE=1024
+CLONE_SKB=1000
+MEASUREMENT_TIME=60
+REST_TIME=5
 RESULT_FILE="result_single_vnf.txt"
 DST_PORT="9201-9205"
 
-for rate in 50 100 200 500 1000 1500 2000 3000 4000 5000 7000 9000; do
+# for rate in 50 100 200 500 1000 1500 2000 3000 4000 5000 7000 9000; do
+for rate in 9000; do
     # start pktgen on background job
     # CIDR for destination IP so that RSS in client can load balance traffic  to queues
-    ./pktgen/pktgen.sh -i ${OUT_NIC} \
-                    -m ${VNF_MAC} \
-                    -d '192.168.4.0/24' \
-                    -p ${DST_PORT} \
-                    -s ${PKT_SIZE} \
-                    -t ${NUM_THREADS} \
-                    -T $(( ${MEASUREMENT_TIME} + 2 * ${REST_TIME} )) \
-                    -B ${rate} \
-                    >> result_single_vnf.txt &
+    sudo ./pktgen/pktgen.sh -i ${OUT_NIC} \
+                            -m ${VNF_MAC} \
+                            -d '192.168.4.0/32' \
+                            -p ${DST_PORT} \
+                            -s ${PKT_SIZE} \
+                            -c ${CLONE_SKB} \
+                            -t ${NUM_THREADS} \
+                            -T $(( ${MEASUREMENT_TIME} + 2 * ${REST_TIME} )) \
+                            -B ${rate} \
+                            -f 1 \
+                            >> result_single_vnf.txt &
 
     # wait a little for traffic and VNF to stable, then start measurement
-    sleep 3
+    sleep 2
     echo "Traffic rate is ${rate} Mbps" >> ${RESULT_FILE}
-    ssh ${VNF_SSH_ADDR} "sleep $(( ${REST_TIME} - 3 )) && mpstat ${MEASUREMENT_TIME} 1" >> ${RESULT_FILE}
+    ssh ${VNF_SSH_ADDR} "sleep $(( ${REST_TIME} - 2 )) && mpstat ${MEASUREMENT_TIME} 1" >> ${RESULT_FILE}
     sleep ${REST_TIME}
 
     # wait more to ensure pktgen is stopped
-    sleep ${REST_TIME}
+    sleep 2
 
     # empty line to make result file more readable
     echo "" >> ${RESULT_FILE}
